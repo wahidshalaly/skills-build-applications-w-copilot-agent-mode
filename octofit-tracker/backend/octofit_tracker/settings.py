9b467b91c1bed5c998e9222ff6fa0a0e9c95cb54
Wahid Shalaly <194151+wahidshalaly@users.noreply.github.com>
2025-04-09 17:16:42 +1000

    Update settings.py
    Add `djongo` to `INSTALLED_APPS`

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "djongo",
     "rest_framework",
     "corsheaders",
     "octofit_tracker",
